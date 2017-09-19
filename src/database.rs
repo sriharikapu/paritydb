7b0b7b1793a036f0e294c41af8ae92912e04332e
Tomasz Drwięga <tomusdrw@gmail.com>
2017-09-19 10:55:54 +0200

    Creating new database.

--- src/database.rs
+++ src/database.rs
@@ -3,6 +3,7 @@ use std::path::Path;
 
 use error::Result;
 use journal::Journal;
+// use memmap::{Mmap, Protection};
 use transaction::Transaction;
 
 #[derive(Default, Debug, PartialEq)]
@@ -27,8 +28,16 @@ pub struct Database {
 }
 
 impl Database {
+	/// Creates new database at given location.
+	pub fn create<P: AsRef<Path>>(_path: P, _options: Options) -> Result<Self> {
+		unimplemented!()
+
+	}
+
+	/// Opens an existing DB at given location.
 	pub fn open<P: AsRef<Path>>(path: P, options: Options) -> Result<Self> {
 		let journal = Journal::open(&path)?;
+		// let _mmap = Mmap::open_path(&path, Protection::Read)?;
 
 		Ok(Database {
 			journal,
@@ -36,15 +45,16 @@ impl Database {
 		})
 	}
 
-	pub fn apply(&mut self, tx: &Transaction) -> Result<()> {
+	/// Commits changes in the transaction.
+	pub fn commit(&mut self, tx: &Transaction) -> Result<()> {
 		self.journal.push(tx)?;
 		Ok(())
 	}
 
 	/// Flushes up to `max` excessive journal eras to the disk.
-	pub fn flush_journal(&mut self, max: Option<usize>) -> Result<()> {
+	pub fn flush_journal<T: Into<Option<usize>>>(&mut self, max: T) -> Result<()> {
 		let len = self.journal.len();
-		let max = max.unwrap_or(len);
+		let max = max.into().unwrap_or(len);
 
 		if len < self.options.journal_eras {
 			return Ok(())
@@ -83,25 +93,32 @@ mod tests {
 		let temp = tempdir::TempDir::new("create_insert_and_query").unwrap();
 
 		let mut db = Database::open(temp.path(), Options::with(|mut options| {
-			options.journal_eras = 10;
+			options.journal_eras = 0;
 		})).unwrap();
 
 		let mut tx = Transaction::default();
 		tx.insert("abc", "xyz");
 		tx.insert("cde", "123");
 
-		db.apply(&tx).unwrap();
+		db.commit(&tx).unwrap();
 
 		assert_eq!(db.get("abc").unwrap(), b"xyz");
 		assert_eq!(db.get("cde").unwrap(), b"123");
 
+		// Another transaction
 		let mut tx = Transaction::default();
 		tx.insert("abc", "456");
 		tx.delete("cde");
 
-		db.apply(&tx).unwrap();
+		db.commit(&tx).unwrap();
 
 		assert_eq!(db.get("abc").unwrap(), b"456");
+		// assert_eq!(db.get("cde"), None); // from DB
+
+		// Flush journal and fetch everything from DB.
+		// db.flush_journal(2).unwrap();
+
+		// assert_eq!(db.get("abc").unwrap(), b"456");
 		// assert_eq!(db.get("cde"), None);
 	}
 }
