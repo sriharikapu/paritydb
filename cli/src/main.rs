6da1a9fa084adca412e6bc940ea24490b1608037
Guanqun Lu <guanqun.lu@gmail.com>
2017-10-17 14:17:46 +0800

    add get subcommand

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -15,6 +15,23 @@ fn read_parameters<'a>(matches: &'a ArgMatches) -> Result<(&'a str, &'a str, Opt
 	}
 }
 
+fn do_get(db: &str, key: &str) -> Result<(), Error> {
+	let db = Database::open(db, Options::default())?;
+	let ret = db.get(key);
+	match ret {
+		Ok(Some(value)) => {
+			println!("value: {:?}", value);
+		},
+		Ok(None) => {
+			println!("value not found.");
+		},
+		Err(err) => {
+			println!("no value found for this key with error: {:?}.", err);
+		}
+	}
+	Ok(())
+}
+
 fn do_insert(db: &str, key: &str, value: &str) -> Result<(), Error> {
 	let mut db = Database::open(db, Options::default())
 				.or(Database::create(db, Options::default()))?;
@@ -40,6 +57,16 @@ fn main() {
 			.version("0.1.0")
 			.author("Parity Technology")
 			.about("A simple command line interface for ParityDB")
+			.subcommand(SubCommand::with_name("get")
+				.about("Get value from the specified key in database")
+				.arg(Arg::with_name("KEY")
+					.short("k")
+					.long("key")
+					.takes_value(true))
+				.arg(Arg::with_name("DB")
+					.short("d")
+					.long("db")
+					.takes_value(true)))
 			.subcommand(SubCommand::with_name("insert")
 				.about("Insert key to database")
 				.arg(Arg::with_name("KEY")
@@ -67,6 +94,13 @@ fn main() {
 			.get_matches();
 
 	match matches.subcommand() {
+		("get", Some(sub_m)) => {
+			if let Ok((db, key, _)) = read_parameters(&sub_m) {
+				do_get(db, key).expect("execute get error");
+			} else {
+				println!("errors for get.");
+			}
+		},
 		("insert", Some(sub_m)) => {
 			if let Ok((db, key, Some(value))) = read_parameters(&sub_m) {
 				do_insert(db, key, value).expect("execute insert error.");
